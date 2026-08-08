034f137ab799fec13633c129532efad72f0de883
Air <1355471563@qq.com>
2026-08-08 10:19:16 +0800

    chore: bump plugin version to 0.1.4

--- npcink-cloud-addon.php
+++ npcink-cloud-addon.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Npcink Cloud Addon
  * Description:       Cloud connector for Npcink hosted runtime access, signing, health checks, and entitlement summaries.
- * Version:           0.1.3
+ * Version:           0.1.4
  * Requires at least: 7.0
  * Requires PHP:      8.0
  * Author:            Npcink
@@ -29,7 +29,7 @@ if ( ! defined( 'NPCINK_CLOUD_ADDON_DIR' ) ) {
 }
 
 if ( ! defined( 'NPCINK_CLOUD_ADDON_VERSION' ) ) {
-	define( 'NPCINK_CLOUD_ADDON_VERSION', '0.1.3' );
+	define( 'NPCINK_CLOUD_ADDON_VERSION', '0.1.4' );
 }
 
 if ( ! defined( 'NPCINK_CLOUD_ADDON_OPTION_NAME' ) ) {
